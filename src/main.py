8d79f6a36c0e79bb14e0a59f41a7a6c07420e7d9
ruankie <pretoriusr.7@gmail.com>
2023-03-22 22:29:54 +0200

    Added bot

---
+++ src/main.py
@@ -0,0 +1,28 @@
+import os
+import yaml
+import discord
+from dotenv import load_dotenv
+from bot import EngagementBot
+
+
+def main():
+    # load config
+    with open("./bot_config.yml", "r") as f:
+        yaml_content = f.read()
+        config = yaml.safe_load(yaml_content)
+
+    # load env vars
+    load_dotenv()
+    BOT_TOKEN = os.environ["BOT_TOKEN"]
+    CHANNEL_IDS = config["discord"]["channel_ids"]
+
+    # define intents
+    intents = discord.Intents.default()
+    intents.members = True
+
+    # create and run bot
+    bot = EngagementBot(intents=intents, channel_ids=CHANNEL_IDS)
+    engagement_summary = bot.run(BOT_TOKEN)
+
+if __name__ == "__main__":
+    main()
